d671e6608010cd0d0bd300670d1b918891ef1e0c
Oliver Eglseder <php@vxvr.de>
2016-05-14 00:16:13 +0200

    [TASK] Add possibility to disable the backend module and choose its category

--- ext_tables.php
+++ ext_tables.php
@@ -1,13 +1,29 @@
 <?php
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-    'VerteXVaaR.Logs',
-    'tools',
-    'tx_logs_m1',
-    '',
-    ['Log' => 'filter'],
-    [
-        'access' => 'user,group',
-        'icon' => '',
-        'labels' => 'LLL:EXT:logs/Resources/Private/Language/locallang.module.xlf',
-    ]
+call_user_func(
+    function () {
+        if (empty($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['logs'])) {
+            $configuration = [
+                'modules' => '1',
+                'category' => 'tools',
+            ];
+        } else {
+            $configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['logs']);
+        }
+
+        if ('1' === $configuration['modules']) {
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'VerteXVaaR.Logs',
+                $configuration['category'],
+                'tx_logs_m1',
+                '',
+                ['Log' => 'filter'],
+                [
+                    'access' => 'user,group',
+                    'icon' => '',
+                    'labels' => 'LLL:EXT:logs/Resources/Private/Language/locallang.module.xlf',
+                ]
+            );
+        }
+    }
 );
+
