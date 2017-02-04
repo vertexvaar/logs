6e0b858252c8da91d1278bbf3760b9ce42cdaec3
Oliver Eglseder <php@vxvr.de>
2017-02-04 13:30:57 +0100

    [REFACTOR] Reorder ConjunctionReader methods and fields

--- Classes/Log/Reader/ConjunctionReader.php
+++ Classes/Log/Reader/ConjunctionReader.php
@@ -20,6 +20,58 @@ class ConjunctionReader implements ReaderInterface
     ];
 
     /**
+     * @var ReaderInterface[]
+     */
+    protected $readers = [];
+
+    /**
+     * ReaderCollection constructor.
+     *
+     * @param array $configuration
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function __construct(array $configuration = [])
+    {
+    }
+
+    /**
+     * @return Log[]
+     */
+    public function findAll()
+    {
+        $logs = [];
+        foreach ($this->readers as $reader) {
+            $logs = array_merge($logs, $reader->findAll());
+        }
+        return $logs;
+    }
+
+    /**
+     * @param Filter $filter
+     * @return Log[]
+     */
+    public function findByFilter(Filter $filter)
+    {
+        $logs = [];
+        foreach ($this->readers as $reader) {
+            $logs = array_merge($logs, $reader->findByFilter($filter));
+        }
+        $orderField = GeneralUtility::underscoredToUpperCamelCase($filter->getOrderField());
+        $direction = Filter::SORTING_ASC === $filter->getOrderDirection() ? -1 : 1;
+        usort(
+            $logs,
+            function ($left, $right) use ($orderField, $direction) {
+                return $direction
+                       * strcmp(
+                           ObjectAccess::getProperty($right, $orderField),
+                           ObjectAccess::getProperty($left, $orderField)
+                       );
+            }
+        );
+        return array_slice($logs, 0, $filter->getLimit());
+    }
+
+    /**
      * @constructor Factory method to create an instance will all supported log reader
      */
     public static function fromConfiguration()
@@ -28,52 +80,6 @@ class ConjunctionReader implements ReaderInterface
         $conjunctionReader->setReaders(static::getReadersForWriters());
         return $conjunctionReader;
     }
-
-    /**
-     * @param array|null $logConfiguration
-     * @return array
-     */
-    protected static function getReadersForWriters(array $logConfiguration = null)
-    {
-        if (null === $logConfiguration) {
-            $logConfiguration = static::getLogConfiguration();
-        }
-
-        $logReader = [];
-        foreach ($logConfiguration as $key => $value) {
-            if (is_array($value)) {
-                if ('writerConfiguration' !== $key) {
-                    $logReader = array_merge($logReader, static::getReadersForWriters($value));
-                } else {
-                    foreach ($value as $writer) {
-                        if (is_array($writer)) {
-                            foreach ($writer as $class => $writerConfiguration) {
-                                if (isset(static::$writerReaderMapping[$class])) {
-                                    $readerClass = static::$writerReaderMapping[$class];
-                                    $logReader[] = new $readerClass($writerConfiguration);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return $logReader;
-    }
-
-    /**
-     * ReaderCollection constructor.
-     *
-     * @param array $configuration
-     */
-    public function __construct(array $configuration = [])
-    {
-    }
-
-    /**
-     * @var ReaderInterface[]
-     */
-    protected $readers = [];
 
     /**
      * @return ReaderInterface[]
@@ -110,39 +116,35 @@ class ConjunctionReader implements ReaderInterface
     }
 
     /**
-     * @return Log[]
+     * @param array|null $logConfiguration
+     * @return array
      */
-    public function findAll()
+    protected static function getReadersForWriters(array $logConfiguration = null)
     {
-        $logs = [];
-        foreach ($this->readers as $reader) {
-            $logs = array_merge($logs, $reader->findAll());
+        if (null === $logConfiguration) {
+            $logConfiguration = static::getLogConfiguration();
         }
-        return $logs;
-    }
 
-    /**
-     * @param Filter $filter
-     * @return Log[]
-     */
-    public function findByFilter(Filter $filter)
-    {
-        $logs = [];
-        foreach ($this->readers as $reader) {
-            $logs = array_merge($logs, $reader->findByFilter($filter));
-        }
-        $orderField = GeneralUtility::underscoredToUpperCamelCase($filter->getOrderField());
-        $direction = Filter::SORTING_ASC === $filter->getOrderDirection() ? -1 : 1;
-        usort(
-            $logs,
-            function ($left, $right) use ($orderField, $direction) {
-                return $direction * strcmp(
-                    ObjectAccess::getProperty($right, $orderField),
-                    ObjectAccess::getProperty($left, $orderField)
-                );
+        $logReader = [];
+        foreach ($logConfiguration as $key => $value) {
+            if (is_array($value)) {
+                if ('writerConfiguration' !== $key) {
+                    $logReader = array_merge($logReader, static::getReadersForWriters($value));
+                } else {
+                    foreach ($value as $writer) {
+                        if (is_array($writer)) {
+                            foreach ($writer as $class => $writerConfiguration) {
+                                if (isset(static::$writerReaderMapping[$class])) {
+                                    $readerClass = static::$writerReaderMapping[$class];
+                                    $logReader[] = new $readerClass($writerConfiguration);
+                                }
+                            }
+                        }
+                    }
+                }
             }
-        );
-        return array_slice($logs, 0, $filter->getLimit());
+        }
+        return $logReader;
     }
 
     /**
