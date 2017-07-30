36d6850abbcb41bdb3786c28949f7df5d6d601e6
Oliver Eglseder <php@vxvr.de>
2017-07-30 13:57:01 +0200

    [REFACTOR] Move the identification and instantiation of readers and writers to factories

---
+++ Classes/Log/Eraser/EraserFactory.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+namespace VerteXVaaR\Logs\Log\Eraser;
+
+use TYPO3\CMS\Core\Log\Writer\DatabaseWriter;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Class EraserFactory
+ */
+class EraserFactory
+{
+    /**
+     * @var array
+     */
+    protected static $writerEraserMapping = [
+        DatabaseWriter::class => DatabaseEraser::class,
+    ];
+
+    /**
+     * @param array|null $logConfiguration Omit if you want all erasers for all configured writers or pass your writer
+     *     configuration if you want only the erasers for the given writers
+     * @return array
+     */
+    public function getErasersForWriters(array $logConfiguration = null)
+    {
+        if (null === $logConfiguration) {
+            $logConfiguration = $this->getLogConfiguration();
+        }
+
+        $logReader = [];
+        foreach ($logConfiguration as $key => $value) {
+            if (is_array($value)) {
+                if ('writerConfiguration' !== $key) {
+                    $logReader = array_merge($logReader, $this->getErasersForWriters($value));
+                } else {
+                    foreach ($value as $writer) {
+                        if (is_array($writer)) {
+                            foreach ($writer as $class => $writerConfiguration) {
+                                if (isset(static::$writerEraserMapping[$class])) {
+                                    $eraserClass = static::$writerEraserMapping[$class];
+                                    $logReader[] = GeneralUtility::makeInstance($eraserClass, $writerConfiguration);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return $logReader;
+    }
+
+    /**
+     * @return array
+     *
+     * @SuppressWarnings(PHPMD.Superglobals)
+     */
+    protected function getLogConfiguration()
+    {
+        return $GLOBALS['TYPO3_CONF_VARS']['LOG'];
+    }
+}
--- Classes/Log/Reader/ConjunctionReader.php
+++ Classes/Log/Reader/ConjunctionReader.php
@@ -12,13 +12,6 @@ use VerteXVaaR\Logs\Domain\Model\Log;
 class ConjunctionReader implements ReaderInterface
 {
     /**
-     * @var array
-     */
-    protected static $writerReaderMapping = [
-        'TYPO3\\CMS\\Core\\Log\\Writer\\DatabaseWriter' => 'VerteXVaaR\\Logs\\Log\\Reader\\DatabaseReader',
-    ];
-
-    /**
      * @var ReaderInterface[]
      */
     protected $readers = [];
@@ -31,7 +24,8 @@ class ConjunctionReader implements ReaderInterface
      */
     public function __construct(array $configuration = [])
     {
-        $this->readers = $this->getReadersForWriters();
+        $readerFactory = GeneralUtility::makeInstance(ReaderFactory::class);
+        $this->readers = $readerFactory->getReadersForWriters();
     }
 
     /**
@@ -91,46 +85,5 @@ class ConjunctionReader implements ReaderInterface
     public function addReader(ReaderInterface $reader)
     {
         $this->readers[] = $reader;
-    }
-
-    /**
-     * @param array|null $logConfiguration
-     * @return array
-     */
-    protected function getReadersForWriters(array $logConfiguration = null)
-    {
-        if (null === $logConfiguration) {
-            $logConfiguration = $this->getLogConfiguration();
-        }
-
-        $logReader = [];
-        foreach ($logConfiguration as $key => $value) {
-            if (is_array($value)) {
-                if ('writerConfiguration' !== $key) {
-                    $logReader = array_merge($logReader, $this->getReadersForWriters($value));
-                } else {
-                    foreach ($value as $writer) {
-                        if (is_array($writer)) {
-                            foreach ($writer as $class => $writerConfiguration) {
-                                if (isset(static::$writerReaderMapping[$class])) {
-                                    $readerClass = static::$writerReaderMapping[$class];
-                                    $logReader[] = new $readerClass($writerConfiguration);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return $logReader;
-    }
-
-    /**
-     * @return array
-     * @SuppressWarnings(PHPMD.Superglobals)
-     */
-    protected function getLogConfiguration()
-    {
-        return $GLOBALS['TYPO3_CONF_VARS']['LOG'];
     }
 }
