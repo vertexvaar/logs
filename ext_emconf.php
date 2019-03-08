a4766fcbd5d311974adb93888792a27a490b9de9
Oliver Eglseder <php@vxvr.de>
2019-03-08 08:52:17 +0100

    [META] Raise dependencies (TYPO3 to v8 LTS and PHP to 7.0)

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.1.0-9.99.99',
+            'typo3' => '8.7.0-9.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
