6eabb2aad62f10441c62ac019402be823ee57642
Oliver Eglseder <oliver.eglseder@co-stack.com>
2020-05-01 14:26:42 +0200

    [RELEASE] Version 3.1 with full TYPO3 v10 compat, unique readers and atomatic QA

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'co-stack.com Logs',
-    'description' => 'Sophisticated Log Reader API with a backend module to red, filter and delete logs from the TYPO3 Logging API',
+    'description' => 'Log Reader with a backend module and API to read, filter and delete logs from the TYPO3 Logging API',
     'category' => 'module',
     'state' => 'stable',
     'uploadfolder' => 0,
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Oliver Eglseder',
     'author_email' => 'oliver.eglseder@co-stack.com',
     'author_company' => 'co-stack.com',
-    'version' => '3.0.1',
+    'version' => '3.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
